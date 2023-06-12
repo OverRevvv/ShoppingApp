841357b0a9bc5c9dedebe05610e6a92f28d1ba3d
OverRevvv <rdude1612@gmail.com>
2023-06-12 20:21:10 +0530

    configuration for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -4,21 +4,24 @@ import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import path from 'path';
 import history from 'connect-history-api-fallback';
-const port = 8000;
 
+const port = 8000;
+const mongoAtlas = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.gaiffox.mongodb.net/?retryWrites=true&w=majority`;
+const mongoLocal = 'mongodb://127.0.0.1:27017';
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors({
     origin: 'http://localhost:5173',
 }));
-app.use('/images', express.static(path.join(__dirname, '../assets')));
 
-app.use(express.static(path.resolve(__dirname,'../dist'),{maxAge: '1y', etag: false}));
+app.use('/images', express.static(path.join(__dirname, '../assets')));
+app.use(express.static(path.resolve(__dirname, '../dist'), { maxAge: '1y', etag: false }));
 app.use(history());
 
 app.get('/api/products', async (req, res) => {
     const client = await MongoClient.connect(
-        'mongodb://127.0.0.1:27017',
+        mongoLocal,
         { useNewUrlParser: true, useUnifiedTopology: true },
     );
     const db = client.db('shoeDb');
@@ -26,10 +29,11 @@ app.get('/api/products', async (req, res) => {
     res.status(200).json(products);
     client.close();
 });
+
 app.get('/api/users/:userId/cart', async (req, res) => {
     const { userId } = req.params;
     const client = await MongoClient.connect(
-        'mongodb://127.0.0.1:27017',
+        mongoLocal,
         { useNewUrlParser: true, useUnifiedTopology: true },
     );
     const db = client.db('shoeDb');
@@ -41,10 +45,11 @@ app.get('/api/users/:userId/cart', async (req, res) => {
     res.status(200).json(cartItems);
     client.close();
 });
+
 app.get('/api/products/:productId', async (req, res) => {
     const { productId } = req.params;
     const client = await MongoClient.connect(
-        'mongodb://127.0.0.1:27017',
+        mongoLocal,
         { useNewUrlParser: true, useUnifiedTopology: true },
     );
     const db = client.db('shoeDb');
@@ -56,11 +61,12 @@ app.get('/api/products/:productId', async (req, res) => {
     }
     client.close();
 })
+
 app.post('/api/users/:userId/cart', async (req, res) => {
     const { userId } = req.params;
     const { productId } = req.body;
     const client = await MongoClient.connect(
-        'mongodb://127.0.0.1:27017',
+        mongoLocal,
         { useNewUrlParser: true, useUnifiedTopology: true },
     );
     const db = client.db('shoeDb');
@@ -79,7 +85,7 @@ app.post('/api/users/:userId/cart', async (req, res) => {
 app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
     const { userId, productId } = req.params;
     const client = await MongoClient.connect(
-        'mongodb://127.0.0.1:27017',
+        mongoLocal,
         { useNewUrlParser: true, useUnifiedTopology: true },
     );
     const db = client.db('shoeDb');
@@ -94,7 +100,7 @@ app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
     client.close();
 })
 
-app.get('*',(req, res)=>{
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
 
