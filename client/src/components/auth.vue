602dd7ac99e9d27d0868aeea497e2fb6c2362fa0
OverRevvv <rdude1612@gmail.com>
2023-08-30 19:07:21 +0530

    squashed commits
    trying
    
    tring 2.0
    
    little bit css fix with new client build
    
    cleaned unncessary code from server.js
    
    little bit style fix
    
    some changes again
    
    trying to debug issue
    
    little changes in the build while fixing issue
    
    lazy loading pages client app
    
    lazy loading fix
    
    lazy fix
    
    trying lazy again
    
    trying again to fix the lazy loading issue
    
    lazy loading is messing up regardless so i am removing it from client app
    
    skeleton loaders for products page
    
    removed some dev files
    
    added lazy loading for productDetails page
    
    added lazy loading again
    
    trying background image
    
    little bit more UI improvement
    
    little bit improvement for landing page's skeleton loader
    
    made skeleton for cart page
    
    little bit css changes
    
    skeleton update for products details page
    
    Squashed 24 commits
    
    another skeleton style change
    
    Squashed 27 commits in 1
    
    trying
    
    tring 2.0
    
    little bit css fix with new client build
    
    cleaned unncessary code from server.js
    
    little bit style fix
    
    some changes again
    
    trying to debug issue
    
    little changes in the build while fixing issue
    
    lazy loading pages client app
    
    lazy loading fix
    
    lazy fix
    
    trying lazy again
    
    trying again to fix the lazy loading issue
    
    lazy loading is messing up regardless so i am removing it from client app
    
    skeleton loaders for products page
    
    removed some dev files
    
    added lazy loading for productDetails page
    
    added lazy loading again
    
    trying background image
    
    little bit more UI improvement
    
    little bit improvement for landing page's skeleton loader
    
    made skeleton for cart page
    
    little bit css changes
    
    skeleton update for products details page
    
    Squashed 24 commits
    
    another skeleton style change

---
+++ client/src/components/auth.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login-signup-form">
+    <div class="login-form">
+      <h1>Login</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        v-model="loginEmail"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        v-model="loginPassword"
+      />
+      <button @click="login">Login</button>
+    </div>
+    <div class="signup-form">
+      <h1>Sign Up</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        v-model="signupEmail"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        v-model="signupPassword"
+      />
+      <button @click="signup">Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useState } from 'vue';
+
+const loginEmail = useState('');
+const loginPassword = useState('');
+const signupEmail = useState('');
+const signupPassword = useState('');
+
+const login = async () => {
+  // TODO: Implement login logic
+};
+
+const signup = async () => {
+  // TODO: Implement signup logic
+};
+</script>
