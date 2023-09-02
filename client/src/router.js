9d838d5df15178fd8245254a4b67a348fb3f46bb
OverRevvv <rdude1612@gmail.com>
2023-09-02 13:19:32 +0530

    trying to debug issue

--- client/src/router.js
+++ client/src/router.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import productPage from './views/products.vue'
-// import productDetail from './views/productDetails.vue'
-// import cartPage from './views/cart.vue'
-// import notFound from './views/notFound.vue'
+import productDetail from './views/productDetails.vue'
+import cartPage from './views/cart.vue'
+import notFound from './views/notFound.vue'
 
 const routes = [
   { path: '/', redirect: '/products' },
   { path: '/products', name: "products", component: productPage, },
-  { path: '/products/:id', name: "productDetails", component: ()=> import('./views/productDetails.vue') },
-  { path: '/cart', name: "cart", component:()=> import('./views/cart.vue') },
-  { path: '/:pathMatch(.*)', name: "notFound", component:()=> import('./views/notFound.vue') },
+  { path: '/products/:id', name: "productDetails", component: productDetail },
+  { path: '/cart', name: "cart", component: cartPage },
+  { path: '/:pathMatch(.*)', name: "notFound", component: notFound },
 ];
 
 const router = createRouter({
