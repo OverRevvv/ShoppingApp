7f5896806aa33168aaf04a68cd7ba11bf071d3c4
OverRevvv <rdude1612@gmail.com>
2023-09-04 14:29:51 +0530

    trying lazy again

--- client/src/router.js
+++ client/src/router.js
@@ -1,13 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import productPage from './views/products.vue'
-import productDetail from './views/productDetails.vue'
-import cartPage from './views/cart.vue'
-import notFound from './views/notFound.vue'
+import productPage from './views/products.vue';
+// import productDetail from './views/productDetails.vue';
+// import cartPage from './views/cart.vue';
+// import notFound from './views/notFound.vue';
 
+const productDetail = () => import('./views/productDetails.vue');
+const cartPage = () => import('./views/cart.vue');
+const notFound = () => import('./views/notFound.vue');
 
 const routes = [
   { path: '/', redirect: '/products' },
-  { path: '/products', name: "products", component: productPage, },
+  { path: '/products', name: "products", component: productPage},
   { path: '/products/:id', name: "productDetails", component: productDetail },
   { path: '/cart', name: "cart", component: cartPage },
   { path: '/:pathMatch(.*)', name: "notFound", component: notFound },
