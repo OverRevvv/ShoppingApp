04ea6bceab77b4aef58d51592bb3fc560b9dbebf
OverRevvv <rdude1612@gmail.com>
2023-09-16 21:35:38 +0530

    skeleton update for products details page

--- client/src/views/productDetails.vue
+++ client/src/views/productDetails.vue
@@ -18,7 +18,7 @@ const getData = async () => {
     product.value = results.data;
     setTimeout(() => {
         inProgress.value = false;
-    }, 500);
+    }, 900);
     const cartResults = await axios.get(`${baseUrl}/users/1459/cart`);
     cartItems.value = cartResults.data;
 }
@@ -38,36 +38,38 @@ const itemIsInCart = computed(() => {
 })
 </script>
 <template>
-    <div id="page-wrap" v-show="inProgress">
-        <div class="skeleton-image" />
-        <div id="product-details">
-            <div class="skeleton-name" />
-            <div class="skeleton-price" />
-            <div class="skeleton-rating" />
-            <div class="skeleton-button" />
-            <div class="skeleton-desc" />
-            <div v-for="n in 10" :key="n" class="skeleton-para" />
-            <div class="skeleton-para-small" />
+    <div id="page-wrap">
+        <div class="skeleton-wrap" v-show="inProgress">
+        <!-- <div class="skeleton-wrap"> -->
+            <div class="skeleton-image blur" />
+            <div id="product-details">
+                <div class="skeleton-name blur" />
+                <div class="skeleton-price blur" />
+                <div class="skeleton-rating blur" />
+                <div class="skeleton-button blur " />
+                <div class="skeleton-desc blur" />
+                <div v-for="n in 11" :key="n" class="skeleton-para blur" />
+                <div class="skeleton-para-small blur" />
+            </div>
         </div>
+        <div class="prod-wrap" v-if="!inProgress">
+            <div class="img-wrap">
+                <img :src="product.imageUrl" />
+            </div>
+            <div id="product-details">
+                <h1 class="blur">{{ product.name }}</h1>
+                <h3 id="price" class="blur">${{ product.price }}</h3>
+                <p class="blur"> Average Rating: {{ product.averageRating }}</p>
+                <button v-if="!successMessage && !itemIsInCart" id="add-to-cart" @click="addToCart">Add to Cart</button>
+                <button v-if="successMessage && !itemIsInCart" class="green-button" id="add-to-cart">Successfully Added item
+                    to the cart!</button>
+                <button v-if="itemIsInCart" class="grey-button" id="add-to-cart">Item is already added</button>
+                <h4 class="blur">Description</h4>
+                <p class="blur">{{ product.description }}</p>
+            </div>
+        </div>
+        <NotFound v-show="!product" />
     </div>
-    <div id="page-wrap" v-if="product">
-        <div class="img-wrap">
-            <img :src="product.imageUrl" />
-        </div>
-        <div id="product-details">
-            <h1 class="blur">{{ product.name }}</h1>
-            <h3 id="price" class="blur">${{ product.price }}</h3>
-            <p class="blur"> Average Rating: {{ product.averageRating }}</p>
-            <button v-if="!successMessage && !itemIsInCart" id="add-to-cart" @click="addToCart">Add to Cart</button>
-            <button v-if="successMessage && !itemIsInCart" class="green-button" id="add-to-cart">Successfully Added item
-                to
-                the cart!</button>
-            <button v-if="itemIsInCart" class="grey-button" id="add-to-cart">Item is already added</button>
-            <h4 class="blur">Description</h4>
-            <p class="blur">{{ product.description }}</p>
-        </div>
-    </div>
-    <NotFound v-else />
 </template>
 <style scoped>
 #page-wrap {
@@ -75,13 +77,19 @@ const itemIsInCart = computed(() => {
     padding: 16px;
     max-width: 600px;
     position: relative;
+    height: 160vh;
 }
 
 .skeleton-wrap {
-    margin-top: 16px;
-    padding: 16px;
     max-width: 600px;
+    width: 94.8%;
+    height: 100%;
+    z-index: 10;
     position: absolute;
+}
+
+.prod-wrap {
+    position: relative;
 }
 
 .img-wrap {
@@ -95,7 +103,6 @@ img {
 
 #product-details {
     padding: 16px;
-    position: relative;
 }
 
 #add-to-cart {
@@ -117,34 +124,37 @@ img {
 }
 
 .skeleton-name {
-    background-color: #646363;
-    height: 2.8rem;
-    width: 80%;
+    background-color: #64636367;
+    height: 3.4rem;
+    width: 100%;
     border-radius: 8px;
-    margin: 9% auto;
+    margin: 0 auto;
+    margin-top: 8%;
 }
 
 .skeleton-price {
-    background-color: #646363;
-    height: 2rem;
-    width: 20%;
+    background-color: #64636367;
+    height: 1.8rem;
+    width: 100%;
     border-radius: 8px;
-    margin: 5% auto;
+    margin: 0% auto;
+    margin-top: 6%;
 }
 
 .skeleton-rating {
-    background-color: #646363;
+    background-color: #64636367;
     height: 1.6rem;
-    width: 60%;
+    width: 100%;
     border-radius: 4px;
-    margin: 6% auto;
+    margin: 0% auto;
+    margin-top: 4%;
 }
 
 .skeleton-image {
     height: 400px;
     width: 400px;
-    background-color: #646363;
-    border-radius: 8px;
+    background-color: #646363d8;
+    border-radius: 1rem;
     margin: 0 auto;
 }
 
@@ -154,31 +164,33 @@ img {
     border-radius: 8px;
     border: 1px solid transparent;
     padding: 0.6em 1.2em;
-    background-color: #000000;
+    background-color: #111111a4;
     margin: 0 auto;
+    margin-top: 2.6%;
 }
 
 .skeleton-desc {
-    background-color: #646363;
-    height: 1rem;
-    width: 30%;
+    background-color: #64636367;
+    height: 1.4rem;
+    width: 100%;
     border-radius: 4px;
-    margin: 6% auto;
+    margin: 4.1% auto;
 }
 
 .skeleton-para {
-    background-color: #646363;
-    height: .7rem;
+    background-color: #64636367;
+    height: 1.2rem;
     width: 100%;
     border-radius: 4px;
-    margin: 7.5px auto;
+    margin: 0.010rem auto;
 }
 
 .skeleton-para-small {
-    background-color: #646363;
-    height: .7rem;
+    background-color: #64636367;
+    height: 1.5rem;
     width: 50%;
     border-radius: 4px;
-    margin: 7.5px auto;
+    margin: 0 auto;
+    margin-top: 0.3rem;
 }
 </style>
