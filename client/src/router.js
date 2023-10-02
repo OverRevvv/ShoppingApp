cfb4f50a8c6f3bd503ae08085f23322923c24b3e
OverRevvv <rdude1612@gmail.com>
2023-10-02 20:23:44 +0530

    auth in progress

--- client/src/router.js
+++ client/src/router.js
@@ -3,12 +3,14 @@ import productPage from './views/products.vue';
 import productDetail from './views/productDetails.vue';
 import cartPage from './views/cart.vue';
 import notFound from './views/notFound.vue';
+import auth from './views/auth.vue';
 
 const routes = [
   { path: '/', redirect: '/products' },
   { path: '/products', name: "products", component: productPage},
   { path: '/products/:id', name: "productDetails", component: productDetail },
   { path: '/cart', name: "cart", component: cartPage },
+  { path: '/auth', name: "cart", component: auth },
   { path: '/:pathMatch(.*)', name: "notFound", component: notFound },
 ];
 
