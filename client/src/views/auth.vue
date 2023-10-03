b35ce9c5f565f2d1e1b36dd0d74a8dacdc6a5df7
OverRevvv <rdude1612@gmail.com>
2023-10-03 22:54:44 +0530

    initialized login/signup component

--- client/src/views/auth.vue
+++ client/src/views/auth.vue
@@ -1,38 +1,12 @@
-<template>
-  <div class="login-signup-form">
-    <div class="login-form">
-      <h1>Login</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        v-model="loginEmail"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        v-model="loginPassword"
-      />
-      <button @click="login">Login</button>
-    </div>
-    <div class="signup-form">
-      <h1>Sign Up</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        v-model="signupEmail"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        v-model="signupPassword"
-      />
-      <button @click="signup">Sign Up</button>
-    </div>
-  </div>
-</template>
-
 <script setup>
 // import { useState } from 'vue';
+import { ref } from 'vue';
+const isHidden1 = ref(true);
+const isHidden2 = ref(true);
+const inputRef1 = ref(null);
+const inputRef2 = ref(null);
+const btnColor1 = ref(null);
+const btnColor2 = ref(null);
 
 // const loginEmail = useState('');
 // const loginPassword = useState('');
@@ -46,4 +20,122 @@ const login = async () => {
 const signup = async () => {
   // TODO: Implement signup logic
 };
+
+const visibiltiy1 = () => {
+  if (inputRef1.value.type == "password") {
+    inputRef1.value.type = "text"
+    btnColor1.value.style.color = "#70fa00";
+    isHidden1.value = false
+  }
+  else {
+    inputRef1.value.type = "password"
+    btnColor1.value.style.color = "#ff1909";
+    isHidden1.value = true
+  }
+}
+const visibiltiy2 = () => {
+  if (inputRef2.value.type == "password") {
+    inputRef2.value.type = "text"
+    btnColor2.value.style.color = "#70fa00";
+    isHidden2.value = false
+  }
+  else {
+    inputRef2.value.type = "password"
+    btnColor2.value.style.color = "#ff1909";
+    isHidden2.value = true
+  }
+}
 </script>
+<template>
+  <div class="container">
+    <div class="login-form authBox blur">
+      <h1>Login</h1>
+      <label>Email</label>
+      <input type="email" placeholder="enter your email" v-model="loginEmail" required />
+      <label>Password</label>
+      <input type="password" placeholder="enter your password" v-model="loginPassword" ref="inputRef1" required />
+      <i class="fa" @click="visibiltiy1" ref="btnColor1">{{ isHidden1 ? "" : "" }}</i>
+      <button @click="login">Login</button>
+    </div>
+    <div class="signup-form blur authBox">
+      <h1>Sign Up</h1>
+      <label>Email</label>
+      <input type="email" placeholder="enter your email" v-model="signupEmail" required />
+      <label>Password</label>
+      <input type="password" placeholder="enter your password" v-model="signupPassword" ref="inputRef2" required />
+      <i class="fa" @click="visibiltiy2" ref="btnColor2">{{ isHidden2 ? "" : "" }}</i>
+      <button @click="signup">Sign Up</button>
+    </div>
+  </div>
+  <!-- <i class="fa"></i> -->
+  <!-- <i class="fa">&#xf070;</i> -->
+  <!-- <i class="fa">&#xf06e;</i> -->
+</template>
+<style scoped>
+.container {
+  height: 80vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.authBox {
+  height: 80%;
+  background-color: #11111170;
+  width: 40%;
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 16px;
+}
+.authBox h1{
+  font-size: 2.5rem;
+}
+
+.authBox h1 {
+  top: -0.35rem;
+  position: absolute;
+}
+
+label {
+  font-size: 1.5rem;
+  margin-top: 1rem;
+  text-shadow: 0px 0px 2px #f7f7f7;
+}
+
+input {
+  background-color: #da52254b;
+  border: 1px solid black;
+  text-align: center;
+  border-radius: 8px;
+  margin: 0.25rem;
+  font-size: 1.1rem;
+  position: relative;
+  height: 10%;
+  width: 80%;
+}
+
+input::placeholder {
+  color: black;
+  font-size: 1.1rem;
+  text-shadow: 0px 0px 1px #111111;
+}
+
+i {
+  color: #ff1909 ;
+  text-shadow: 0px 0px 20px #f7f7f7;
+  margin-top: 5px;
+  font-size: 1.4rem;
+  margin-bottom: 2rem;
+  cursor: pointer;
+  user-select: none;
+}
+
+button {
+  bottom: 3.3rem;
+  position: absolute;
+}
+</style>
